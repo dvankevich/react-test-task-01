1247f3addbdf7bbca9b2ce88fccedf6ed3d59b4c
dvankevich <dvankevich@gmail.com>
2026-01-20 21:10:23 +0200

    add persist store

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 import "./index.css";
 import App from "./components/App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate
+        loading={<div>Loading saved data...</div>}
+        persistor={persistor}
+      >
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { campersReducer } from './campers/slice';
-import { filtersReducer } from './filters/slice';
-import { favoritesReducer } from './favorites/slice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { campersReducer } from "./campers/slice";
+import { filtersReducer } from "./filters/slice";
+import { favoritesReducer } from "./favorites/slice";
+
+const persistConfig = {
+  key: "campers",
+  storage,
+  whitelist: ["favorites"], // Зберігаємо тільки список обраного
+};
+
+const rootReducer = combineReducers({
+  campers: campersReducer,
+  filters: filtersReducer,
+  favorites: favoritesReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    campers: campersReducer,
-    filters: filtersReducer,
-    favorites: favoritesReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ігноруємо системні екшени redux-persist, щоб не було помилок у консолі
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
