6e0ad1f2b7cfc6953c6196b736e2c4ece0603bd2
dvankevich <dvankevich@gmail.com>
2026-01-23 20:25:36 +0200

    add CampersList component

---
+++ src/components/CamperList/CamperList.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import VehicleCard from "../VehicleCard/VehicleCard";
+import { selectCampers, selectIsLoading } from "../../redux/selectors";
+import styles from "./CamperList.module.css";
+
+const CamperList = () => {
+  const campers = useSelector(selectCampers);
+  const isLoading = useSelector(selectIsLoading);
+
+  const [visibleCount, setVisibleCount] = useState(4);
+  const [prevCampers, setPrevCampers] = useState(campers);
+
+  // Скидання стану ПІД ЧАС рендеру
+  if (campers !== prevCampers) {
+    setPrevCampers(campers);
+    setVisibleCount(4);
+  }
+
+  const visibleCampers = campers.slice(0, visibleCount);
+  const hasMore = visibleCount < campers.length;
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 4);
+  };
+
+  if (isLoading && campers.length === 0) {
+    return <p className={styles.loading}>Loading campers...</p>;
+  }
+
+  if (!isLoading && campers.length === 0) {
+    return (
+      <div className={styles.noResults}>
+        <h3>No campers found for these filters 😢</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.list}>
+        {visibleCampers.map((camper) => (
+          <VehicleCard key={camper.id} camper={camper} />
+        ))}
+      </div>
+
+      {hasMore && (
+        <button className={styles.loadMoreBtn} onClick={handleLoadMore}>
+          Load more
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default CamperList;
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,50 +1,13 @@
-import { useSelector } from "react-redux";
-import { useState } from "react";
 import FilterSidebar from "../../components/FilterSidebar/FilterSidebar";
-import VehicleCard from "../../components/VehicleCard/VehicleCard";
-import { selectCampers, selectIsLoading } from "../../redux/selectors";
+import CamperList from "../../components/CamperList/CamperList";
 import styles from "./CatalogPage.module.css";
 
 const CatalogPage = () => {
-  const campers = useSelector(selectCampers);
-  const isLoading = useSelector(selectIsLoading);
-
-  const [visibleCount, setVisibleCount] = useState(4);
-  const visibleCampers = campers.slice(0, visibleCount);
-  const hasMore = visibleCount < campers.length;
-
-  const handleLoadMore = () => {
-    setVisibleCount((prev) => prev + 4);
-  };
-
   return (
     <main className={styles.container}>
-      {/* Ліва частина: Фільтри */}
       <FilterSidebar />
-
-      {/* Права частина: Список та кнопка */}
-      <section className={styles.catalogSection}>
-        {isLoading && campers.length === 0 ? (
-          <p>Loading...</p>
-        ) : (
-          <div className={styles.list}>
-            {visibleCampers.length > 0 ? (
-              visibleCampers.map((camper) => (
-                <VehicleCard key={camper.id} camper={camper} />
-              ))
-            ) : (
-              <div className={styles.noResults}>
-                <h3>No campers found for these filters.</h3>
-              </div>
-            )}
-          </div>
-        )}
-
-        {hasMore && !isLoading && (
-          <button className={styles.loadMoreBtn} onClick={handleLoadMore}>
-            Load more
-          </button>
-        )}
+      <section>
+        <CamperList />
       </section>
     </main>
   );
