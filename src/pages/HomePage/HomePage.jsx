95e7cd2dfc95b46bc5b19bcc5c15ee148d4a8038
dvankevich <dvankevich@gmail.com>
2026-01-25 15:44:22 +0200

    add HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./HomePage.module.css";
+
 const HomePage = () => {
+  const navigate = useNavigate();
+
   return (
-    <div style={{ padding: "40px 64px", textAlign: "center" }}>
-      <h1>Welcome to TravelTrucks! 🚐</h1>
-      <p>Find your best camper for the journey.</p>
-    </div>
+    <section className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Campers of your dreams</h1>
+          <p className={styles.subtitle}>
+            You can find everything you want in our catalog
+          </p>
+          <button
+            className={styles.button}
+            onClick={() => navigate("/catalog")}
+          >
+            View Now
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
