423495687fd3b82bc5119a859b9f0953e2c465bb
dvankevich <dvankevich@gmail.com>
2026-01-29 17:18:14 +0200

    remove CamperList component

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,15 +1,27 @@
 import FilterSidebar from "../../components/FilterSidebar/FilterSidebar";
-import CamperList from "../../components/CamperList/CamperList";
+//import CamperList from "../../components/CamperList/CamperList";
 import styles from "./CatalogPage.module.css";
+import VehicleList from "../../components/VehicleList/VehicleList";
+import { useSelector } from "react-redux";
+import { selectCampers, selectIsLoading } from "../../redux/selectors";
 
 const CatalogPage = () => {
+  const campers = useSelector(selectCampers);
+  const isLoading = useSelector(selectIsLoading);
+
   return (
     <>
       <title>Catalog</title>
       <main className={styles.container}>
         <FilterSidebar />
         <section>
-          <CamperList />
+          {/* <CamperList /> */}
+
+          <VehicleList
+            items={campers}
+            isLoading={isLoading}
+            emptyMessage="No campers found for these filters 😢"
+          />
         </section>
       </main>
     </>
