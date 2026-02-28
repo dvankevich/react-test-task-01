0e8b6c88ed21dafbfede29551f11414352695536
dvankevich <dvankevich@gmail.com>
2026-02-28 15:54:38 +0200

    fix: remove head tag from SEO component

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const SEO = ({ title, description, image, url }) => {
   return (
-    <head>
+    <>
       <title>{title ? `${title} | TravelTrucks` : "TravelTrucks"}</title>
       <meta name="description" content={description} />
 
@@ -17,7 +17,7 @@ const SEO = ({ title, description, image, url }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-    </head>
+    </>
   );
 };
 
