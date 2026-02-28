12c36bc07d36ace5d35df437690029d607bb4a0c
dvankevich <dvankevich@gmail.com>
2026-02-28 15:36:55 +0200

    refactor: camper operations

--- src/redux/campers/operations.js
+++ src/redux/campers/operations.js
@@ -5,8 +5,12 @@ axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
 
 export const fetchCampers = createAsyncThunk(
   "campers/fetchAll",
-  async (filters = {}, thunkAPI) => {
+  // thunkAPI must be second argument; disable default-param-last warning
+  // eslint-disable-next-line default-param-last
+  async (filters, thunkAPI) => {
     try {
+      // normalize filters (default to empty object)
+      filters = filters || {};
       // clear filters
       const params = Object.entries(filters).reduce((acc, [key, value]) => {
         if (value !== "" && value !== false && value !== null) {
@@ -21,7 +25,7 @@ export const fetchCampers = createAsyncThunk(
     } catch (error) {
       // handle error 404
       // return empty items
-      if (error.response && error.response.status === 404) {
+      if (error.response?.status === 404) {
         return { items: [], total: 0 };
       }
       return thunkAPI.rejectWithValue(error.message);
